56dae115e096a4493e289abd2e8041230312e55f
sarahkittyy <chrisohlin1@gmail.com>
2019-05-18 23:15:34 -0400

    added floor and ceiling rect

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main()
 {
 	//app window
-	sf::RenderWindow window(sf::VideoMode(500, 500), "Raycasting 2");
+	sf::RenderWindow window(sf::VideoMode(1000, 500), "Raycasting 2");
 	sf::Clock appClock;   //app clock
 
 	////App vars
@@ -16,6 +16,14 @@ int main()
 
 	Player p({200, 500}, &m);   //map player
 	////
+
+	//Background colors.
+	sf::RectangleShape ceil, floor;
+	ceil.setFillColor(sf::Color(90, 90, 90));
+	ceil.setSize({500, 250});
+	floor.setFillColor(sf::Color(50, 50, 50));
+	floor.setSize({500, 250});
+	floor.setPosition({0, 250});
 
 	while (window.isOpen())
 	{
@@ -42,8 +50,17 @@ int main()
 		//clear
 		window.clear(sf::Color(127, 127, 127));
 
+		//Draw the floor and ceil
+		window.draw(floor);
+		window.draw(ceil);
+
 		//Draw the player's POV.
 		window.draw(p);
+
+		sf::RenderStates s;
+		s.transform.translate({500, 0});
+		window.draw(m, s);
+		p.draw2D(window, s);
 
 		//display
 		window.display();
